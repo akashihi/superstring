89715728b24a5f70bf39132756b091f8fbf6e69a
Denis Chaplygin <akashihi@gmail.com>
2019-06-23 21:16:21 +0300

    Added string list loading and preprocessing

--- genetic/build.sbt
+++ genetic/build.sbt
@@ -3,3 +3,5 @@ name := "genetic"
 version := "0.1"
 
 scalaVersion := "2.13.0"
+
+libraryDependencies += "org.scala-lang.modules" %% "scala-parallel-collections" % "0.2.0"
--- genetic/src/main/scala/Genetic.scala
+++ genetic/src/main/scala/Genetic.scala
@@ -1,3 +1,36 @@
+import collection.parallel._
+
 object Genetic {
 
+  type SuffixMap = Map[Char, Seq[Int]]
+  case class MappedString(str: String, suffixMap: SuffixMap)
+
+  def buildSuffixMap(str: String): SuffixMap =
+    str.zipWithIndex.foldLeft(Map[Char, Seq[Int]]())((sm, ci) => {
+      val (char, index) = ci
+      sm + (char -> sm.getOrElse(char, Seq[Int]()).:+(index))
+    })
+
+  def main(args: Array[String]): Unit = {
+    if (args.isEmpty) {
+      System.err.println("Usage: genetic <file_with_strings>")
+      return
+    }
+
+    // 0 - Load the file
+    val bufferedSource = io.Source.fromFile(args.head)
+    val inputStrings = (for (line <- bufferedSource.getLines()) yield line).toList.distinct
+    bufferedSource.close()
+    System.out.println(s"Loaded ${inputStrings.size} words")
+
+    //1 - Drop shorter words which are contained in a longer ones
+    System.out.println("Dropping contained words")
+    val nonContainedStrings = inputStrings.toParArray.filterNot(outer => inputStrings.exists(inner => outer != inner && inner.contains(outer)))
+
+    //2 - Calculate suffix map for each dataset entry
+    System.out.println("Building suffix map")
+    val mappedStrings = nonContainedStrings.map(str => MappedString(str, buildSuffixMap(str))).toList
+
+    //3 - Run genetic algorithm on the string array
+  }
 }
